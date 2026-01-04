511d14c62bf525fc2d98ed4f2a226ea9798bb278
ashinberish <ashinberish@gmail.com>
2026-01-05 02:07:37 +0530

    added frontend with some initial scalffolding

---
+++ tracker-fe/src/App.tsx
@@ -0,0 +1,12 @@
+import { ComponentExample } from "@/components/component-example";
+import Layout from "@/components/layout";
+
+export function App() {
+  return (
+    <Layout>
+      <ComponentExample />
+    </Layout>
+  );
+}
+
+export default App;
---
+++ tracker-fe/src/components/app-sidebar.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import { Command, CreditCard, Home } from "lucide-react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavUser } from "@/components/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Home",
+      url: "#",
+      icon: Home,
+      isActive: true,
+    },
+    {
+      title: "Expenses",
+      url: "#",
+      icon: CreditCard,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar
+      className="top-(--header-height) h-[calc(100svh-var(--header-height))]!"
+      {...props}
+    >
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">Expense Tracker</span>
+                  <span className="truncate text-xs">Track your expenses</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
